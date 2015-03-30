6272f78d89b6d455cab40934e4ddf4a2f78e1a4f
Andrew Varnerin <andrew@varnerin.info>
2015-03-30 19:43:12 -0400

    simplify using amqp.channel

--- geocoder/app.js
+++ geocoder/app.js
@@ -1,44 +1,23 @@
 require('dotenv').load({'path': '../.env'});
-var open = require('amqplib').connect(process.env.AMQP_URL);
 
-var createChannel = function(conn) {
-  return conn.createChannel();
-};
-
-var createExchange = function(ch) {
-  var ok = ch.assertExchange('fileUploads', 'topic', {durable: true}).then(function() {
-    return ch;
-  });
-  return ok;
-};
-
-var createQueue = function(ch) {
-  var ok = ch.assertQueue('fileUploads', {durable: true}).then(function(queue) {
-    return {queue: queue.queue, ch: ch };
-  });
-  return ok;
-};
-
-var bindQueue = function(qok){
-  var ok = qok.ch.bindQueue(qok.queue, 'fileUploads', 'fileUploaded').then(function() {
-    return qok;
-  });
-  return ok;
-};
-
-var listen = function(qok) {
-  var listener = function(data) {
-    console.log(data.content.toString());
-    qok.ch.ack(data)
+var listen = function(ch) {
+  var listener = function(parsed, data) {
+    console.log(parsed);
+    ch.ack(data)
   };
 
-  console.log('using queue ' + qok.queue);
-  qok.ch.consume(qok.queue, listener);
+  console.log('using queue fileUploads');
+  ch.consume('fileUploads', listener);
 };
 
-open
-  .then(createChannel)
-  .then(createExchange)
-  .then(createQueue)
-  .then(bindQueue)
-  .then(listen);
+require('amqp.channel')(process.env.AMQP_URL, {
+  assertExchange: [['fileUploads', 'topic', {durable: true}]],
+  assertQueue: [['fileUploads', {durable: true}]],
+  bindQueue: [['fileUploads', 'fileUploads', 'fileUploaded']]
+}).then(listen);
+
+process.once('SIGINT', error);
+
+function error(e){
+  process.exit(1);
+}
