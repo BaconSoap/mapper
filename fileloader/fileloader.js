5b1f8ac53c2f9a45fec31a067b24d3722bafbac3
Andrew Varnerin <andrew@varnerin.info>
2015-03-30 22:24:39 -0400

    load uploaded files into a table

--- fileloader/fileloader.js
+++ fileloader/fileloader.js
@@ -1,10 +1,20 @@
 require('./fileloader.js');
 var db = require('../db/index.js');
+var csv = require('fast-csv');
 
 var listen = function(ch) {
   var listener = function(parsed, data) {
-    console.log(parsed);
-    ch.ack(data)
+    loadFile(parsed.filePath, parsed.fileId, function(fileData) {
+      db.model.FileData.loadFile(fileData.data, fileData.headers, parsed.fileId, function(e) {
+        if (e) {
+          console.error(e);
+        } else {
+          console.log('loaded ' + parsed.filePath + ' to `tmp.' + parsed.fileId + '`');
+          ch.publish('fileLoads', 'fileLoaded', {fileId: parsed.fileId, tableName: 'tmp.' + parsed.fileId});
+        }
+        ch.ack(data)
+      });
+    });
   };
 
   console.log('using queue fileUploads');
@@ -22,3 +32,17 @@ process.once('SIGINT', error);
 function error(e){
   process.exit(1);
 }
+
+var loadFile = function(path, id, cb) {
+  var data = [];
+  csv
+    .fromPath(path)
+    .on('data', function(row) { data.push(row) } )
+    .on('end', function() {
+      var parsed = {};
+      parsed.headers = (data.splice(0, 1))[0];
+      parsed.data = data;
+      console.log(parsed);
+      cb(parsed);
+    });
+};
--- www/public/js/build/app.js
+++ www/public/js/build/app.js
@@ -1,10 +1,8 @@
 jQuery(function () {
     jQuery('#fileupload').fileupload({
         dataType: 'json',
-        done: function (e, data) {
-            jQuery.each(data.result.files, function (index, file) {
-                jQuery('<p/>').text(file.name).appendTo(document.body);
-            });
+        done: function (e) {
+            jQuery('#fileupload').after('file uploaded');
         }
     });
 });
